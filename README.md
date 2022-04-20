b033c6177c6c0807b4755a8245dd93761b3cdf12
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2022-04-19 20:17:54 -0700

    add v3

--- README.md
+++ README.md
@@ -1,43 +1,39 @@
 # GA-Track
 
-[![Build Status](https://travis-ci.org/firstandthird/ga-track.svg?branch=master)](https://travis-ci.org/firstandthird/ga-track)
-<span class="ga-track-npmversion"><a href="https://npmjs.org/package/ga-track" title="View this project on NPM"><img src="https://img.shields.io/npm/v/ga-track.svg" alt="NPM version" /></a></span>
-
 Google Analytics Tracking helper with support for GTag, ga and _gaq.
+
+## V3+ Note
+
+To support a wider range of front-end layout systems (WordPress/Shopify/React) we've removed some features. Legacy sites should stick to v2.
+
+  - Autotrack (`[data-ga-track]`) has been removed. We recommend binding clicks to the `sendEvent()` method.
+  - Outline mode has been removed to reduce dependencies.
+  - `sendEvent()` no longer accepts the callback and timeout arguments.
+  - `getData()` has been removed.
+  - The method to enable debug mode has changed. See docs below.
+  - The method to change the tracker name has changed. See docs below.
 
 ## Installation
 
 ```sh
+// npm
 npm install ga-track
+
+// yarn
+yarn add ga-track
 ```
 
 ## Usage
 
-> Make sure you also include your Google Analytics script as shown [in the example](./example/index.html#L7).
-
 ```js
-import 'ga-track';
-// or
 import GATrack from 'ga-track';
 ```
 
-Any element with `data-ga-track` as an attribute, will be tracked on click. Here's a quick reference:
-
-|  Attribute             | Description  | Default  | 
-|------------------------|-----------------------|---|
-| `data-ga-track`        | Needed for autotracking. If a value is given it serves as category. | `ga-track`  | 
-| `data-ga-track-label`  | Label of the event. |  Element's `href` |
-| `data-ga-track-action` | Action of the event. |  Element's `textContent` |
-| `data-ga-track-href`   | Should this be `false` the link **won't** be navigated to. Otherwise `ga-track` will wait till the track happens and then navigates.  |  Element's `href` |
-
-
 ## Methods
 
-GATrack exposes a few method being the following two the most importants. It is a safe call so if Google Analytics gets blocked by an adblocker it won't throw any exceptions due to not being present.
+### sendEvent(category, action, label)
 
-### sendEvent(category, action, label, callback, timeout)
-
-Manually sends an event to Google Analytics.
+Manually sends an event to Google Analytics. Returns a promise.
 
 #### Parameters:
 
@@ -47,10 +43,6 @@ Manually sends an event to Google Analytics.
 
 `label` - {string} - Event's label.
 
-`[callback]` - {Function} - Callback to be called once the event has been tracked.
-
-`[timeout=1000]` - Timeout after which the callback will be called if it didn't finish.
-
 ### sendData()
 
 Safely pass data to Google Analytics:
@@ -59,36 +51,40 @@ Safely pass data to Google Analytics:
 GATrack.sendData('set', 'dimension2', 'member');
 ```
 
-## Example
+## Options
 
-See the [complete example](./example/index.html).
+### Debug Mode
 
-```html
-<html>
+```js
+import GATrack from 'ga-track';
 
-  ...
+GATrack.debug = true;
+```
 
-  </body>
+### Changing Tracker Name
 
-    ...
-    
-    <span>Click</span>
-    <span>Me</span>
-    <span>One</span>
-    <span>Time</span>
+```js
+import GATrack from 'ga-track';
 
-    <a data-ga-track href="http://www.firstandthird.com">Link</a>
-    <a data-ga-track href="http://www.firstandthird.com" target="_blank">External link</a>
+GATrack.trackerName = 'SomeTrackerName';
+```
 
-    <script>
-      GaTrack.track([
-        {
-          element: 'span',
-          category: 'span',
-          label: 'click'
-        }
-      ])
-    </script>
-  </body>
-</html>
+### Forcing a provider
+
+Supported values: `''`, `'ga'`, `'gaq'`, `'gtag'`
+
+This is useful if you have multiple tags on the same page but only want to use one of them.
+
+```js
+import GATrack from 'ga-track';
+
+GATrack.force = 'ga';
+```
+
+### Category Prefix
+
+```js
+import GATrack from 'ga-track';
+
+GATrack.prefix = 'SiteName'; // category becomes SiteName-categoryvalue
 ```
--- index.js
+++ index.js
@@ -1,225 +1,96 @@
-/* eslint-disable callback-return */
-/* eslint-env browser */
-/* global _gaq, ga, gtag */
-import { on, find, ready, closest, matches } from 'domassist';
-import aug from 'aug';
+/* global window */
 
-const GATrack = {
-  sendEvent(category, action, label, callback = null, timeout = 1000) {
-    if (GATrack.prefix) {
-      category = `${GATrack.prefix}-${category}`;
+class GATrack {
+  static prefix = null;
+  static debug = false;
+  static trackerName = '';
+  static force = null;
+
+  static async sendEvent(category, action, label) {
+    if (this.prefix) {
+      category = `${this.prefix}-${category}`;
     }
-    let onHit = () => {};
-    GATrack.log(category, action, label);
 
-    if (!GATrack.isEnabled) {
-      if (typeof callback === 'function') {
-        callback();
+    return new Promise(resolve => {
+      this.log(category, action, label);
+
+      if (!this.isEnabled()) {
+        return resolve();
       }
 
-      return GATrack;
-    }
+      if (this.isGAQ()) {
+        window._gaq.push(['_trackEvent', category, action, label, null, false]);
+        window._gaq.push(resolve);
+      } else if (this.isGTag()) {
+        // Gtag check needs to go before since gtag creates a ga variable
+        const payload = {
+          event_category: category,
+          event_label: label,
+          event_callback: resolve
+        };
 
-    if (typeof callback === 'function') {
-      let timeoutId;
+        this.sendData('event', action, payload);
+      } else if (this.isGA()) {
+        const options = {
+          transport: 'beacon',
+          hitCallback: resolve
+        };
 
-      onHit = () => {
-        if (!timeoutId) {
-          return;
-        }
+        this.sendData('send', 'event', category, action, label, options);
+      }
+    });
+  }
 
-        clearTimeout(timeoutId);
-        timeoutId = null;
-        callback();
-      };
-
-      timeoutId = setTimeout(onHit, timeout);
-    }
-
-    if (GATrack.isGAQ) {
-      _gaq.push(['_trackEvent', category, action, label, null, false]);
-      _gaq.push(onHit);
-    } else if (GATrack.isGTag) {
-      // Gtag check needs to go before since gtag creates a ga variable
-      const payload = {
-        event_category: category,
-        event_label: label,
-        event_callback: onHit
-      };
-
-      GATrack.sendData('event', action, payload);
-    } else if (GATrack.isGA) {
-      const options = {
-        transport: 'beacon',
-        hitCallback: onHit
-      };
-
-      GATrack.sendData('send', 'event', category, action, label, options);
-    }
-  },
-
-  send() {
-    // eslint-disable-next-line prefer-rest-params
-    const args = Array.prototype.slice.call(arguments);
-
-    if (GATrack.isGA) {
+  static send(...args) {
+    if (this.isGA()) {
       args.unshift('send');
     }
 
-    GATrack.sendData.apply(null, args);
-  },
+    return this.sendData(...args);
+  }
 
-  sendData() {
-    if (!GATrack.isEnabled) {
-      return GATrack;
-    }
-
-    // eslint-disable-next-line prefer-rest-params
-    const args = Array.prototype.slice.call(arguments);
-
-    if (GATrack.isGTag) {
-      gtag.apply(null, args);
-    } else if (GATrack.isGA) {
-      if (GATrack.trackerName) {
-        args[0] = `${GATrack.trackerName}.${args[0]}`;
-      }
-
-      ga.apply(null, args);
-    }
-  },
-
-  getData(element, options = {}) {
-    const href = element.dataset.gaTrackHref || element.getAttribute('href');
-    const category = element.dataset.gaTrack || options.category || 'ga-track';
-    const label = element.dataset.gaTrackLabel || options.label || href;
-    const action = element.dataset.gaTrackAction || options.action || element.textContent.trim();
-
-    return { href, category, label, action };
-  },
-
-  track(element, options = {}) {
-    if (Array.isArray(element)) {
-      element.forEach(data => {
-        find(data.element).forEach(el => {
-          GATrack.track(el, data);
-        });
-      });
-
+  static sendData(...args) {
+    if (!this.isEnabled()) {
       return;
     }
 
-    if (typeof element.dataset.gaTrackInitialised !== 'undefined') {
-      return;
-    }
-
-    element.dataset.gaTrackInitialised = 'true';
-
-    options = aug({}, GATrack.defaults, options);
-
-    this.log('tracking', element, options);
-    on(element, 'click', event => {
-      GATrack.onTrackedClick(element, event, options);
-    });
-  },
-
-  onTrackedClick(element, event, options) {
-    const data = GATrack.getData(element, options);
-    const target = element.getAttribute('target');
-    let callback = null;
-
-    if (element.dataset.gaTrackHref === 'false') {
-      event.preventDefault();
-    } else if (data.href && !event.metaKey && event.which === 1 && target !== '_blank') {
-      event.preventDefault();
-      callback = () => {
-        window.location = data.href;
-      };
-    }
-
-    GATrack.sendEvent(data.category, data.action, data.label, callback, options.timeout);
-  },
-
-  autotrack() {
-    if (GATrack.autotracking === true) {
-      return;
-    }
-
-    GATrack.autotracking = true;
-    const selector = '[data-ga-track]';
-
-    on(document.body, 'click', event => {
-      let element = event.target;
-
-      if (!matches(element, selector)) {
-        element = closest(element, selector);
+    if (this.isGTag()) {
+      window.gtag.apply(null, args);
+    } else if (this.isGA()) {
+      if (this.trackerName) {
+        args[0] = `${this.trackerName}.${args[0]}`;
       }
 
-      if (element) {
-        GATrack.onTrackedClick(element, event, GATrack.defaults);
-      }
-    });
-  },
+      window.ga.apply(null, args);
+    }
+  }
 
-  log(...args) {
-    if (GATrack.debug) {
+  static isNullOrEnforced(provider) {
+    return this.force === null || this.force === provider;
+  }
+
+  static isGAQ() {
+    // eslint-disable-line no-underscore-dangle
+    return (typeof window._gaq !== 'undefined') && this.isNullOrEnforced('gaq');
+  }
+
+  static isGTag() {
+    return (typeof window.gtag !== 'undefined') && this.isNullOrEnforced('gtag');
+  }
+
+  static isGA() {
+    return (typeof window.ga !== 'undefined') && this.isNullOrEnforced('ga');
+  }
+
+  static isEnabled() {
+    return this.isGA || this.isGTag || this.isGAQ;
+  }
+
+  static log(...args) {
+    if (this.debug) {
       console.log('GATRACK', ...args); //eslint-disable-line no-console
     }
-  },
-
-  isNullOrEnforced(provider) {
-    return GATrack.force === null || GATrack.force === provider;
-  },
-
-  get isGAQ() {
-    // eslint-disable-line no-underscore-dangle
-    return (typeof window._gaq !== 'undefined') && GATrack.isNullOrEnforced('gaq');
-  },
-
-  get isGTag() {
-    return (typeof window.gtag !== 'undefined') && GATrack.isNullOrEnforced('gtag');
-  },
-
-  get isGA() {
-    return (typeof window.ga !== 'undefined') && GATrack.isNullOrEnforced('ga');
-  },
-
-  get isEnabled() {
-    return GATrack.isGA || GATrack.isGTag || GATrack.isGAQ;
-  },
-
-  debug: (typeof window.localStorage === 'object' && window.localStorage.getItem('GATrackDebug')),
-  prefix: null,
-  trackerName: '',
-  force: null,
-  defaults: {
-    timeout: 1000
   }
-};
-
-const outlineTracked = () => find('[data-ga-track-initialised], [data-ga-track]').map(el => {
-  el.style.outline = 'red dotted 1px';
-  return el;
-});
-
-GATrack.debug = window.localStorage.getItem('GATrackDebug');
-const outline = window.localStorage.getItem('GATrackOutline');
-
-window.GAOutlineTracked = outlineTracked;
-
-ready(() => {
-  if (!window._GATrack_) {
-    window._GATrack_ = GATrack;
-
-    if (typeof window.gaTrackerName_ !== 'undefined') {
-      GATrack.trackerName = window.gaTrackerName_;
-    }
-
-    GATrack.autotrack();
-
-    if (outline) {
-      outlineTracked();
-    }
-  }
-});
+}
 
 export default GATrack;
